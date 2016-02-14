fcc7595c3bfee9fde1870db870a164d6efd40db1
Christine <cmelasigue@gmail.com>
2016-02-14 20:05:55 +0000

    Lesson 29 Maps Integration

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
 	validates :name, presence: true
+	validates :address, :presence => true
+	validates :address, :presence => true
+	validates :name,:length => { minimum: 3 }, :presence => true
 end
--- config/application.rb
+++ config/application.rb
@@ -27,3 +27,5 @@ module Nomster
 
   end
 end
+
+
